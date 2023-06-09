507528621a5542a298a1a2047e4531d5352227ee
netsec2minutes <53455970+netsec2minutes@users.noreply.github.com>
2023-06-09 02:33:52 -0300

    Update zap_to_sarif.py

--- zap_to_sarif.py
+++ zap_to_sarif.py
@@ -1,16 +1,17 @@
 import os
 import json
 
-def convert_zap_json_to_sarif(json_report):
+def convert_json_to_sarif(json_report):
     sarif_report = {
-        "$schema": "https://json.schemastore.org/sarif-2.1.0-rtm.5.json",
-        "version": "2.1.0",
-        "runs": [
+        "$schema" : "https://schemastore.azurewebsites.net/schemas/json/sarif-2.1.0-rtm.5.json",
+        "version" : "2.1.0",
+        "runs" : [
             {
-                "tool": {
-                    "driver": {
-                        "name": "OWASP ZAP",
-                        "rules": []
+                "tool" : {
+                    "driver" : {
+                        "name" : "DASTARDLY",
+                        "version" : "1.0.0",
+                        "rules" : []
                     }
                 },
                 "results": []
@@ -20,32 +21,37 @@ def convert_zap_json_to_sarif(json_report):
 
     zap_report = json.loads(json_report)
 
-    if 'alerts' not in zap_report:
-        print("No 'alerts' key in the report.")
+    if 'rules' not in zap_report:
+        print("No 'rules' key in the report.")
         return json.dumps(sarif_report)
 
-    for alert in zap_report['alerts']:
-        rule_id = alert['pluginid']
+    for rule in zap_report['rules']:
+        rule_id = rule['id']
         sarif_report['runs'][0]['tool']['driver']['rules'].append({
             "id": rule_id,
-            "name": alert['alert'],
+            "name": rule['name'],
             "shortDescription": {
-                "text": alert['desc']
-            },
-            "helpUri": alert['reference']
+                "text": rule['name']
+            }
         })
 
+    if 'results' not in zap_report:
+        print("No 'results' key in the report.")
+        return json.dumps(sarif_report)
+
+    for result in zap_report['results']:
+        rule_id = result['ruleId']
         sarif_report['runs'][0]['results'].append({
             "ruleId": rule_id,
             "level": "error",
             "message": {
-                "text": alert['alert']
+                "text": result['message']
             },
             "locations": [
                 {
                     "physicalLocation": {
                         "artifactLocation": {
-                            "uri": alert['url']
+                            "uri": result['locations'][0]['physicalLocation']['artifactLocation']['uri']
                         }
                     }
                 }
@@ -61,7 +67,7 @@ if __name__ == "__main__":
     
     with open(zap_report_path, 'r') as zap_report_file:
         zap_report = zap_report_file.read()
-        sarif_report = convert_zap_json_to_sarif(zap_report)
+        sarif_report = convert_json_to_sarif(zap_report)
 
     with open(sarif_report_path, 'w') as sarif_report_file:
         sarif_report_file.write(sarif_report)
