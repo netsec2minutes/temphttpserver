f025c62731ebed0e241f1e900d7f18dd9d8a5697
netsec2minutes <53455970+netsec2minutes@users.noreply.github.com>
2023-06-09 11:56:26 -0300

    Update zap_to_sarif.py

--- zap_to_sarif.py
+++ zap_to_sarif.py
@@ -1,27 +1,38 @@
-import os
 import json
-import argparse
+import sys
 
-def convert_json_to_sarif(json_report):
-    # O código para conversão para SARIF permanece o mesmo
-    ...
+def convert_to_sarif(json_path):
+    # Ler o arquivo JSON de entrada
+    with open(json_path, 'r') as json_file:
+        zap_report = json.load(json_file)
 
+    # Realizar a conversão para SARIF
+    sarif_report = convert_to_sarif_format(zap_report)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-J", "--json_path", help="Path to the OWASP ZAP JSON baseline file", required=True)
-    args = parser.parse_args()
-
-    zap_report_path = args.json_path
-    sarif_report_path = "zap_report.sarif"  # Ajuste o caminho onde deseja salvar o arquivo SARIF resultante
-
-    if os.path.isfile(zap_report_path):
-        with open(zap_report_path, 'r') as zap_report_file:
-            zap_report = zap_report_file.read()
-            sarif_report = convert_json_to_sarif(zap_report)
-
-        with open(sarif_report_path, 'w') as sarif_report_file:
+    # Verificar se o relatório SARIF foi gerado corretamente
+    if sarif_report is not None:
+        # Salvar o relatório SARIF em um arquivo
+        with open('zap_report.sarif', 'w') as sarif_report_file:
             sarif_report_file.write(sarif_report)
+        print("Conversão para SARIF concluída com sucesso.")
     else:
-        print(f"Error: JSON file not found at path: {zap_report_path}")
+        print("Erro ao gerar o relatório SARIF.")
+        sys.exit(1)
 
+def convert_to_sarif_format(zap_report):
+    # Implemente aqui a lógica de conversão do JSON do OWASP ZAP para o formato SARIF
+    # ...
+    # Código de conversão aqui
+    # ...
+    return sarif_report
+
+# Verificar se o caminho do arquivo JSON foi fornecido como argumento
+if len(sys.argv) != 3 or sys.argv[1] != '-J':
+    print("usage: zap_to_sarif.py -J JSON_PATH")
+    sys.exit(2)
+
+# Obter o caminho do arquivo JSON
+json_path = sys.argv[2]
+
+# Chamar a função de conversão
+convert_to_sarif(json_path)
