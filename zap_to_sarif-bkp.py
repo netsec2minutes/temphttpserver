24d5a7d474dade94a8b2ded9666d476f12f90421
netsec2minutes <53455970+netsec2minutes@users.noreply.github.com>
2023-06-09 19:51:21 -0300

    Create zap_to_sarif-bkp.py

---
+++ zap_to_sarif-bkp.py
@@ -0,0 +1,76 @@
+import json
+import argparse
+
+def convert_json_to_sarif(json_report):
+    sarif_report = {
+        "$schema": "https://schemastore.azurewebsites.net/schemas/json/sarif-2.1.0-rtm.5.json",
+        "version": "2.1.0",
+        "runs": [
+            {
+                "tool": {
+                    "driver": {
+                        "name": "OWASP ZAP",
+                        "version": "1.0.0",
+                        "rules": []
+                    }
+                },
+                "results": []
+            }
+        ]
+    }
+
+    if 'site' in json_report:
+        site = json_report['site'][0]
+        if 'alerts' in site:
+            for alert in site['alerts']:
+                rule_id = alert['pluginid']
+                sarif_report['runs'][0]['tool']['driver']['rules'].append({
+                    "id": rule_id,
+                    "name": alert['name'],
+                    "shortDescription": {
+                        "text": alert['alert']
+                    }
+                })
+
+                instances = alert.get('instances', [])
+                for instance in instances:
+                    sarif_report['runs'][0]['results'].append({
+                        "ruleId": rule_id,
+                        "level": "error",
+                        "message": {
+                            "text": alert['desc']
+                        },
+                        "locations": [
+                            {
+                                "physicalLocation": {
+                                    "artifactLocation": {
+                                        "uri": instance['uri']
+                                    }
+                                }
+                            }
+                        ]
+                    })
+
+    return sarif_report
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-J", "--json_path", help="Path to the OWASP ZAP JSON baseline file")
+    args = parser.parse_args()
+
+    zap_report_path = args.json_path
+    sarif_report_path = "zap_report.sarif"  # Ajuste o caminho onde deseja salvar o arquivo SARIF resultante
+
+    if zap_report_path is None:
+        print("ERROR: Path to JSON file is missing.")
+        exit(1)
+
+    with open(zap_report_path, 'r') as zap_report_file:
+        zap_report = json.load(zap_report_file)
+        sarif_report = convert_json_to_sarif(zap_report)
+
+    with open(sarif_report_path, 'w') as sarif_report_file:
+        json.dump(sarif_report, sarif_report_file)
+
+    print("Conversion to SARIF completed successfully.")
