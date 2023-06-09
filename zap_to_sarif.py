454500356663b3bfbae6bb6c875bf33383ca6f71
netsec2minutes <53455970+netsec2minutes@users.noreply.github.com>
2023-06-09 11:23:13 -0300

    Update zap_to_sarif.py

--- zap_to_sarif.py
+++ zap_to_sarif.py
@@ -11,13 +11,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-J", "--json_path", help="Path to the OWASP ZAP JSON baseline file", required=True)
     args = parser.parse_args()
-    
+
     zap_report_path = args.json_path
     sarif_report_path = "zap_report.sarif"  # Ajuste o caminho onde deseja salvar o arquivo SARIF resultante
 
-    with open(zap_report_path, 'r') as zap_report_file:
-        zap_report = zap_report_file.read()
-        sarif_report = convert_json_to_sarif(zap_report)
+    if os.path.isfile(zap_report_path):
+        with open(zap_report_path, 'r') as zap_report_file:
+            zap_report = zap_report_file.read()
+            sarif_report = convert_json_to_sarif(zap_report)
 
-    with open(sarif_report_path, 'w') as sarif_report_file:
-        sarif_report_file.write(sarif_report)
+        with open(sarif_report_path, 'w') as sarif_report_file:
+            sarif_report_file.write(sarif_report)
+    else:
+        print(f"Error: JSON file not found at path: {zap_report_path}")
